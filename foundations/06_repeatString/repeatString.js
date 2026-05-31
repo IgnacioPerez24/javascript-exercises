f18f7f2cf4c7e94b82bb0c4c8e9a67971ea569d0
Ignacio Perez <Ignacio.Perez.Pereyro@gmail.com>
2026-05-30 21:49:42 -0300

    Update repeatString.js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let repetedString = "";
+    if(number < 0) return "ERROR";
 
+    for(let i = 0 ; i < number; i++){
+        repetedString += word;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
