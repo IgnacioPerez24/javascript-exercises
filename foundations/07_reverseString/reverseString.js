a892dbe83bb160589d322eece09efbcc34f4ffac
Nacho <ignacio.perez.pereyro@gmail.com>
2026-05-30 22:05:10 -0300

    07 Reverse string Exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let revWord = "";
+    let arrWord = word.split("");
+    let revArr = arrWord.reverse();
+    return revWord = revArr.join("");
 };
 
 // Do not edit below this line
