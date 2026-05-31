23f0315397da2e07a4ad8dc9dce5c8391fbd7b2c
Nacho <ignacio.perez.pereyro@gmail.com>
2026-05-30 21:59:29 -0300

    06 - Repeat String Exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,11 +1,12 @@
 const repeatString = function(word, number) {
+
     let repetedString = "";
     if(number < 0) return "ERROR";
 
     for(let i = 0 ; i < number; i++){
         repetedString += word;
     }
-    return repeatString;
+    return repetedString;
 };
 
 // Do not edit below this line
