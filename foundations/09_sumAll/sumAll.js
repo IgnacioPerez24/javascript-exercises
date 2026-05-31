bfa55d61c9cd570b1d9daa505bce75a6e398141c
Nacho <ignacio.perez.pereyro@gmail.com>
2026-05-30 22:25:24 -0300

    09 Sum all Exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(numInit,numEnd) {
 
+    if(!Number.isInteger(numInit) || !Number.isInteger(numEnd)) return "ERROR"; 
+
+    let total = 0;
+    for(let i = numInit; i <= numEnd; i++)
+    {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
